2fc7fd47120a1154819b0ea0911b6550315801b7
otterji <youngsdg1@gmail.com>
2023-05-23 20:32:21 +0900

    feat: 사진 업로드 테스트

--- src/App.js
+++ src/App.js
@@ -1,95 +1,83 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const canvasRef = useRef(null);
+  const [images, setImages] = useState([]);
+  const [isDragging, setIsDragging] = useState(false);
+  const [draggedImageIndex, setDraggedImageIndex] = useState(null);
+  const [previousPosition, setPreviousPosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-  
-    // Variables to store the image position and dragging state
-    let imageX = 50;
-    let imageY = 50;
-    let isDragging = false;
-    let previousX = 0;
-    let previousY = 0;
-  
-    // Create an image object
-    const image = new Image();
-    image.src = '/soodal.png'; // Replace 'image.jpg' with the actual filename and extension of your image
-  
-    // Draw the image on the canvas once it has loaded
-    image.onload = () => {
-      drawImage();
-  
-      // Add event listeners for mouse down, mouse move, and mouse up
-      canvas.addEventListener('mousedown', handleMouseDown);
-      canvas.addEventListener('mousemove', handleMouseMove);
-      canvas.addEventListener('mouseup', handleMouseUp);
-    };
-  
-    // Function to draw the image at the current position
-    function drawImage() {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.drawImage(image, imageX, imageY, 100, 100); // Adjust the dimensions as needed
-    }
-  
-    function handleMouseDown(event) {
-      const offsetX = event.offsetX;
-      const offsetY = event.offsetY;
-      if (
-        offsetX >= imageX &&
-        offsetX <= imageX + 100 && // Adjust the dimensions as needed
-        offsetY >= imageY &&
-        offsetY <= imageY + 100 // Adjust the dimensions as needed
-      ) {
-        isDragging = true;
-        previousX = offsetX;
-        previousY = offsetY;
-      }
-    }
-    
-  
-    // Function to handle the mouse move event
-    function handleMouseMove(event) {
-      if (isDragging) {
-        const offsetX = event.offsetX;
-        const offsetY = event.offsetY;
-        const dx = offsetX - previousX;
-        const dy = offsetY - previousY;
-        imageX += dx;
-        imageY += dy;
-        previousX = offsetX;
-        previousY = offsetY;
-        drawImage();
-      }
-    }
-    
-  
-    // Function to handle the mouse up event
-    function handleMouseUp() {
-      isDragging = false;
-    }
-  
-    // Clean up event listeners when the component unmounts
-    return () => {
-      canvas.removeEventListener('mousedown', handleMouseDown);
-      canvas.removeEventListener('mousemove', handleMouseMove);
-      canvas.removeEventListener('mouseup', handleMouseUp);
-    };
-  }, []);
-  
-  
+  function handleImageUpload(event) {
+    const uploadedImages = Array.from(event.target.files);
+    const readerPromises = uploadedImages.map((image) => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (e) => resolve(e.target.result);
+        reader.onerror = (error) => reject(error);
+        reader.readAsDataURL(image);
+      });
+    });
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    // Your canvas configuration and drawing code goes here
-  }, []);
+    Promise.all(readerPromises)
+      .then((results) => {
+        const newImages = results.map((result) => ({ src: result, position: { x: 0, y: 0 } }));
+        setImages((prevImages) => [...prevImages, ...newImages]);
+      })
+      .catch((error) => {
+        console.log('Error uploading images:', error);
+      });
+  }
+
+  function handleMouseDown(event, index) {
+    setIsDragging(true);
+    setDraggedImageIndex(index);
+    setPreviousPosition({ x: event.clientX, y: event.clientY });
+  }
+
+  function handleMouseMove(event) {
+    if (isDragging && draggedImageIndex !== null) {
+      const dx = event.clientX - previousPosition.x;
+      const dy = event.clientY - previousPosition.y;
+
+      setPreviousPosition({ x: event.clientX, y: event.clientY });
+
+      setImages((prevImages) => {
+        const updatedImages = [...prevImages];
+        const draggedImage = updatedImages[draggedImageIndex];
+
+        if (draggedImage && draggedImage.position) {
+          draggedImage.position.x += dx;
+          draggedImage.position.y += dy;
+        }
+
+        return updatedImages;
+      });
+    }
+  }
+
+  function handleMouseUp() {
+    setIsDragging(false);
+    setDraggedImageIndex(null);
+  }
 
   return (
     <div>
-      <canvas ref={canvasRef} width={500} height={500} />
+      <input type="file" accept="image/*" multiple onChange={handleImageUpload} />
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image.src}
+          alt="Uploaded"
+          style={{
+            position: 'absolute',
+            top: image.position.y,
+            left: image.position.x,
+            cursor: 'move',
+          }}
+          onMouseDown={(event) => handleMouseDown(event, index)}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+        />
+      ))}
     </div>
   );
 }
